483a5b7c51f81d040b3e674fbce6dd2295a92928
BabaVasss <134033816+BabaVasss@users.noreply.github.com>
2024-02-26 13:03:45 -0500

    Assignment one completed

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -2,7 +2,7 @@ console.log("hello")
 
 const string = "string";
 const number = 0;
-const someBoolean = true; // false;
+const someBoolean = true; 
 const array = [ "one", "two", "three" ];
 const obj = {
   key: 'value',
@@ -16,7 +16,7 @@ const data = [
   { key: "value" },
 ];
 
-// console.log(data);
+
 
 let changeableGlobal = true;
 const constantGlobal = true;
@@ -26,39 +26,23 @@ changeableGlobal = false;
 var something = false;
 
 function changeEmUp() {
-  // this will work
+ 
   changeableGlobal = false;
-  // console.log('changeableGlobal :>> ', changeableGlobal);
-  // this won't work, and will throw an error in your console and stop the script
-  // constantGlobal = false;
+  
 }
 changeEmUp();
-// console.log('changeableGlobal :>> ', changeableGlobal);
 
-
-// const mapTest = array.map(d => d + " thing")
-// console.log(mapTest)
-
-// let mapTestTwo = [];
-// array.forEach(d => {
-//   // mapTestTwo.push(d +" thing")
-//   console.log(d)
-// })
-// console.log(mapTestTwo)
 
 const testOne = array.map(d => {
-  // console.log("map", d)
-  // return d + " thing"
-  // return d.concat(" thing")
+  
   return `${d} thing`
 })
 const testTwo = array.forEach(d => {
-  // console.log("forEach", d)
+  
   return d
 })
 
-// console.log(testOne)
-// console.log(testTwo)
+
 
 const label = document.getElementById("name-label");
 const input = document.getElementById("name-input")
@@ -73,22 +57,19 @@ function updateName() {
   console.log("clicked")
   console.log(input.value);
 
-  // userName = userName + 1;
-  userName = input.value
+  userName = userName + 1;
+  
+  console.log(userName)
+  
 
-  // traditional
-  // let stringToSay = null;
-  // if (input.value === "ellie"){
-  //   stringToSay = `good luck teaching ${input.value}`
-  // } else {
-  //   stringToSay = `welcome to class ${input.value}`
-  // }
+
 
   // ternarie
   const stringToSay = userName === "Savys Bar and Bristro" 
     ? `Welcome to a burger spot in New York`
     : `welcome to a burger spot outside of New York`
 
+  window.alert(userName)
   window.alert(stringToSay)
 
   label.innerHTML = "The Burger Joint you entered is" + userName + ". You can change it here if you would like to as well: "
