3d371f1d2ead89eb9338eb24d01ba882c4a5fdc4
BabaVasss <134033816+BabaVasss@users.noreply.github.com>
2024-03-06 22:49:41 -0500

    Working graph

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -19,30 +19,39 @@ d3.csv('../data/MoMA_topTenNationalities.csv', d3.autoType).then(data => {
       .style("overflow", "visible");
 
     //add xscale for the bargraph
-    const xScale = d3.scaleBand()
-      .domain(data.map(d => d.Nationality))
-      .range([margin, width - margin])
-      .paddingInner(0.1)
+    //const xScale = d3.scaleBand()
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.Count)])
+      //.domain(data.map(d => d.Nationality)) <- #vertical Bars
+      //.range([margin, width - margin]) <- #vertical Bars
+      .range([margin.left, width - margin.right])
+      //.interpolate(d3.interpolateRound)
+      //.range([margin.left, width + margin.left])
+      //.paddingInner(0.1)
       //.marginInner(0.1)
-      .paddingOuter(0.2)
+      //.paddingOuter(0.2)
       //.marginOuter(0.2)
-
-
-
-      //Below are some of my attempts to create a horizontal graph
     //const xScale = d3.scaleLinear()
-      //.domain([data.map(d => d.Nationality)])
-      //.range([margin.left, width - margin.right])
+      //.domain([0, d3.max(d => d.Count)])
+      //.rangeRound([])
+      
+
 
 
 
     //add yscale for the bargraph // the error is in the yScale
-    const yScale = d3.scaleLinear()
-      .domain([239,5181])
+    const yScale = d3.scaleBand()
+      //.domain([239,5181]) <- #Vertical Bars
+      .domain(data.map(d => d.Nationality))
+      //.domain(data.map(d => d.Nationality))
       //.doman([0, Math.max(...data.map(d => d.Count))])
       //.domain([0,1])
       //.domain([ydomainMin, ydomainMax])
-      .range([height - margin, margin])
+      //.range([height - margin, margin]) <- #Vertical Bars
+      .range([margin.top, height - margin.bottom])
+      //.range([height + margin.top, margin.top])
+      .padding(0.1)
+      .round(true)
       
       
 
@@ -80,4 +89,5 @@ d3.csv('../data/MoMA_topTenNationalities.csv', d3.autoType).then(data => {
       .attr("y", d => yScale(d.Count))
       .attr("width", xScale.bandwidth)
       .attr("height", d => height - yScale(d.Count) - margin)
+      //.attr("height", d => height + margin.top - yScale(d.Count))
   });
