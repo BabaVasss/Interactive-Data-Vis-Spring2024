40f2124c552606aacde172472754baee0912068c
BabaVasss <134033816+BabaVasss@users.noreply.github.com>
2024-03-23 18:54:46 -0400

    completed

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -4,11 +4,9 @@
  margin = { top: 20, bottom: 50, left: 60, right: 60 }
 
 /* LOAD DATA */
-d3.csv("../data/Dogecoin Historical Data.csv", d3.autoType)
+d3.csv('../data/BTC-USD.csv', d3.autoType)
   .then(data => {
-    console.log(data)
-
-    ///Date format for set is month/day/year 
+    console.log('data :>>' ,data)
 
 
 //  console.log(d3.extent(data, d => d.year))
@@ -18,60 +16,73 @@ const xScale = d3.scaleTime()
   .range([margin.left, width - margin.right])
 
 const yScale = d3.scaleLinear()
-  .domain(d3.extent(data, d => d.Price))
+  .domain(d3.extent(data, d => d.High))
   .range([height - margin.bottom, margin.top])
 
- // CREATE SVG ELEMENT
+
+//CREATE SVG ELEMENT
 const svg = d3.select("#container")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
 
- // BUILD AND CALL AXES
+//BUILD AND CALL AXES
  const xAxis = d3.axisBottom(xScale)
-  //.tickFormat(d3.timeFormatLocale(Date"%-m/%-d/%Y"))
-
   svg.append("g")
   .attr("class", "xAxis")
   .attr("transform", `translate(0,${height - margin.bottom})`)
   .call(xAxis);
   
-  const yAxis = d3.axisLeft(yScale)
+const yAxis = d3.axisLeft(yScale)
   svg.append("g")
     .attr("class", "yAxis")
     .attr("transform", `translate(${margin.left},0)`)
     .call(yAxis);
 
- // LINE GENERATOR FUNCTION
- const lineGen = d3.line()
-    .x(d => xScale(d.Date))
-    .y(d => yScale(d.Price))
-  console.log(lineGen)
+//LINE GENERATOR FUNCTION
+ const line = d3.line()
+   .x(d => xScale(d.Date))
+   .y(d => yScale(d.High))
+  
 
- //const test = lineGen([
- // { Date: new Date(2022, 0, 1), Price: 0.064781},
- // { Date: new Date(2023, 0, 1), Price: 0.071116 }
- //])
-
-
-// console.log(test)
-
- //Area Generator
-
-const area =d3.area()
+//Area Generator
+const area = d3.area()
  .x(d => xScale(d.Date))
- .y0(height - margin.bottom)
- .y1(d => yScale(d.Price))
+ .y0(yScale(0))
+ .y(d => yScale(d.High))
 
- svg.selectAll(".area")
-  .data(data)
-  .join("path")
-  .attr("class", "area")
-  .attr("fill", "purple")
-  .attr("d", (area(data)))
-  .attr("x", d => xScale(d.Date))
-  .attr("y", d => yScale(d.Price))
 
- 
+//Draw Line
+svg.append("path")
+  .attr("stroke", "black")
+  .attr("d", line(data));
+  
 
-});
+//Append Area
+svg.append("path")
+   .attr("fill", "purple")
+   .attr("d", area(data))
+
+
+  //Add labels
+svg.append("text")
+  .attr("class", "xlabel")
+  .attr("x", width / 2)
+  .attr("y", height - margin.bottom / 6)
+  .attr("text-anchor", "middle")
+  .text("Year")
+  .style("font-size", "10px", "bold")
+  .style("fill", "black");
+
+svg.append("text")
+  .attr("class", "ylabel")
+  .attr("x", -height / 2)
+  .attr("y", margin.left / 4)
+  .attr("transform", "rotate(-90)")
+  .attr("text-anchor", "middle")
+  .text("Price-High")
+  .style("font-size", "10px", "bold")
+  .style("fill", "black");
+
+  
+})
