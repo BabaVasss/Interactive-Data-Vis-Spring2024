fbed25d174246611a7e0e878e1f9f6259b6c1803
BabaVasss <134033816+BabaVasss@users.noreply.github.com>
2024-03-23 02:49:57 -0400

    incomplete

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,77 @@
- /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+   /* CONSTANTS AND GLOBALS */
+ const width = window.innerWidth * 0.7,
+ height = window.innerHeight * 0.7,
+ margin = { top: 20, bottom: 50, left: 60, right: 60 }
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
-  return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
-  }
-}).then(data => {
-  console.log('data :>> ', data);
+d3.csv("../data/Dogecoin Historical Data.csv", d3.autoType)
+  .then(data => {
+    console.log(data)
 
-  // SCALES
+    ///Date format for set is month/day/year 
 
-  // CREATE SVG ELEMENT
 
-  // BUILD AND CALL AXES
+//  console.log(d3.extent(data, d => d.year))
+ // + SCALES
+const xScale = d3.scaleTime()
+  .domain(d3.extent(data, d => d.Date))
+  .range([margin.left, width - margin.right])
 
-  // LINE GENERATOR FUNCTION
+const yScale = d3.scaleLinear()
+  .domain(d3.extent(data, d => d.Price))
+  .range([height - margin.bottom, margin.top])
 
-  // DRAW LINE
+ // CREATE SVG ELEMENT
+const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+ // BUILD AND CALL AXES
+ const xAxis = d3.axisBottom(xScale)
+  //.tickFormat(d3.timeFormatLocale(Date"%-m/%-d/%Y"))
+
+  svg.append("g")
+  .attr("class", "xAxis")
+  .attr("transform", `translate(0,${height - margin.bottom})`)
+  .call(xAxis);
+  
+  const yAxis = d3.axisLeft(yScale)
+  svg.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(yAxis);
+
+ // LINE GENERATOR FUNCTION
+ const lineGen = d3.line()
+    .x(d => xScale(d.Date))
+    .y(d => yScale(d.Price))
+  console.log(lineGen)
+
+ //const test = lineGen([
+ // { Date: new Date(2022, 0, 1), Price: 0.064781},
+ // { Date: new Date(2023, 0, 1), Price: 0.071116 }
+ //])
+
+
+// console.log(test)
+
+ //Area Generator
+
+const area =d3.area()
+ .x(d => xScale(d.Date))
+ .y0(height - margin.bottom)
+ .y1(d => yScale(d.Price))
+
+ svg.selectAll(".area")
+  .data(data)
+  .join("path")
+  .attr("class", "area")
+  .attr("fill", "purple")
+  .attr("d", (area(data)))
+  .attr("x", d => xScale(d.Date))
+  .attr("y", d => yScale(d.Price))
+
+ 
 
 });
